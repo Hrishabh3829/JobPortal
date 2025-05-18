eb7c1a3e960c137bb1b2884b2c279d02754ee2a9
Hrishabh3829 <2200032748@kluniversity.in>
2025-05-18 22:57:04 +0530

    user controller created

---
+++ backend/controllers/user.controller.js
@@ -0,0 +1,196 @@
+import jwt from "jsonwebtoken";
+import { User } from "../models/user.model.js";
+import bcrypt from "bcryptjs"
+
+export const register = async (req, res) => {
+    try {
+        const { fullname, email, phoneNumber, password, role } = req.body;
+        if (!fullname || !email || !phoneNumber || !password || !role) {
+            return res.status(400).json({
+                message: "Something is missing",
+                success: false
+            })
+        }
+        const user = await User.findOne({ email })
+        if (user) {
+            return res.status(400).json({
+                message: "User already exists with this email",
+                success: false
+
+            })
+        }
+
+        const hashedPassword = await bcrypt.hash(password, 10);
+
+        await User.create({
+            fullname,
+            email,
+            phoneNumber,
+            password: hashedPassword,
+            role
+        })
+
+        return res.status(200).json({
+            message: "Account created succesfully.",
+            success: true
+        })
+    } catch (error) {
+        console.log(error)
+        res.status(500).json(
+            {
+                message: "Internal server error.",
+                succes: false
+            }
+        )
+
+    }
+}
+
+
+
+export const login = async (req, res) => {
+    try {
+        const { email, password, role } = req.body
+        if (!email || !password || !role) {
+            return res.status(400).json({
+                message: "Something is missing",
+                success: false
+            })
+        }
+
+        let user = await User.findOne({ email })
+        if (!user) {
+            return res.status(400).json({
+                message: "Incorrect Email or Password.",
+                success: false
+            })
+        }
+
+        const isPasswordMatch = await bcrypt.compare(password, user.password)
+        if (!isPasswordMatch) {
+            return res.status(400).json({
+                message: "Incorrect Password or Email.",
+                success: false
+            })
+        }
+
+        //check role correct or not
+        if (role != user.role) {
+            return res.status(400).json({
+                message: "Account doesn't exist with current role.",
+                status: false
+            })
+        }
+
+        const tokenData = {
+            userId: user._id
+        }
+        const token = await jwt.sign(tokenData, process.env.SECRET_KEY, { expiresIn: '1d' })
+
+        user = {
+            _id: user._id,
+            fullname: user.fullname,
+            email: user.email,
+            phoneNumber: user.phoneNumber,
+            role: user.role,
+            profile: user.profile
+
+        }
+
+        return res.status(200).cookie("token", token, { maxAge: 1 * 24 * 60 * 60 * 1000, httpsOnly: true, sameSite: 'strict' }).json({
+            message: `Welcome back ${user.fullname}`,
+            user,
+            success: true
+        })
+
+
+
+
+
+
+    } catch (error) {
+        console.log(error)
+        res.status(500).json(
+            {
+                message: "Internal server error.",
+                succes: false
+            }
+        )
+
+    }
+}
+
+
+
+export const logout = async (req, res) => {
+    try {
+        return res.status(200).cookie("token", "", { maxAge: 0 }).json({
+            message: "Logged out Successfully.",
+            success: true
+        })
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+
+export const updateProfile = async (req, res) => {
+    try {
+        const { fullname, email, phoneNumber, bio, skills } = req.body;
+        const file = req.file;
+        if (!fullname || !email || !phoneNumber || !bio || !skills) {
+            return res.status(400).json({
+                message: "Something is missing",
+                success: false
+            })
+        }
+
+        //cloudinary will come
+
+        const skillsArray = skills.split(",");
+
+        const userId = req.id;//middleware authetication
+        let user = await User.findById({ userId })
+
+        if (!user) {
+            return res.status(400).json({
+                message: "User not found.",
+                success: false
+
+            })
+        }
+        //updating data
+        user.fullname = fullname,
+            user.email = email,
+            user.phoneNumber = phoneNumber,
+            user.profile.bio = bio,
+            user.profile.skills = skillsArray
+
+        //resume will come afterwards
+
+        await user.save();
+
+        user = {
+            _id: user._id,
+            fullname: user.fullname,
+            email: user.email,
+            phoneNumber: user.phoneNumber,
+            role: user.role,
+            profile: user.profile
+
+        }
+
+        return res.status(200).json({
+            message: "Profile updated Successfully.",
+            user,
+            success: true
+        })
+
+    } catch (error) {
+        console.log(error)
+        return res.status(500).json({
+            message: "Internal server error",
+            success: false
+        })
+    }
+}
