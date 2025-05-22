e145fb6b3661fcd4923c33c31df2d6c71ef4e600
Hrishabh3829 <2200032748@kluniversity.in>
2025-05-22 20:04:40 +0530

    Application controller

--- backend/models/job.model.js
+++ backend/models/job.model.js
@@ -43,10 +43,10 @@ const jobSchema = mongoose.Schema({
         ref: 'User',
         required: true
     },
-    application: {
+    applications: [{
         type: mongoose.Schema.Types.ObjectId,
         ref: 'Application'
-    }
+    }]
 
 
 }, { timestamps: true }
