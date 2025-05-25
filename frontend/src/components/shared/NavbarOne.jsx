7347e999fe704670149e0a777ade0568641c3887
Hrishabh3829 <2200032748@kluniversity.in>
2025-05-26 03:13:39 +0530

    Navbar done

--- frontend/src/components/shared/NavbarOne.jsx
+++ frontend/src/components/shared/NavbarOne.jsx
@@ -13,9 +13,11 @@ const NavbarOne = () => {
         <div className='bg-white'>
             <div className='flex items-center justify-between mx-auto max-w-5xl h-16'>
                 <div>
-                    <h1 className='text-2xl font-bold'>
-                        Job<span className='text-[#F83002]'>Portal</span>
-                    </h1>
+                    <Link to="/">
+                        <h1 className='text-2xl font-bold'>
+                            Job<span className='text-[#F83002]'>Portal</span>
+                        </h1>
+                    </Link>
                 </div>
                 <div className="flex items-center gap-12">
                     <ul className='flex font-medium items-center gap-5'>
