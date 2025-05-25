3601aa59b049502b7af07f7b755de5e7319c0d97
Hrishabh3829 <2200032748@kluniversity.in>
2025-05-26 00:55:49 +0530

    sign up page

---
+++ frontend/src/components/auth/Signup.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import NavbarOne from '../shared/NavbarOne'
+
+export const Signup = () => {
+  return (
+    <div>
+        <NavbarOne/>
+        <div>
+            <form action="">
+                <h1 className='font-bold text-xl mb-5'>SignUp</h1>
+                <div>
+                    
+                </div>
+            </form>
+        </div>
+    </div>
+  )
+}
