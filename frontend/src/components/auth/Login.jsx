720a6701c5fb7fb87a0bdfab08ff48a5380bd692
Hrishabh3829 <2200032748@kluniversity.in>
2025-05-27 14:23:12 +0530

    loading added

--- frontend/src/components/auth/Login.jsx
+++ frontend/src/components/auth/Login.jsx
@@ -8,6 +8,10 @@ import { Link, useNavigate } from 'react-router-dom'
 import { toast } from 'sonner'
 import { USER_API_END_POINT } from '@/utils/constant'
 import axios from 'axios';
+import { useDispatch, useSelector } from 'react-redux'
+import { setLoading } from '../redux/authSlice'
+import store from '../redux/store'
+import { Loader2 } from 'lucide-react'
 
 
 
@@ -24,10 +28,14 @@ export const Login = () => {
   }
   const navigate = useNavigate();
 
+  const dispatch = useDispatch()
+
+  const { loading } = useSelector(store => store.auth)
+
   const submitHandler = async (e) => {
     e.preventDefault();
     try {
-
+      dispatch(setLoading(true))
       const res = await axios.post(`${USER_API_END_POINT}/login`, input, {
         headers: {
           "Content-Type": "application/json"
@@ -42,6 +50,8 @@ export const Login = () => {
     } catch (error) {
       console.log(error)
       toast.error(error.response.data.message)
+    } finally {
+      (setLoading(false))
     }
   }
 
@@ -98,9 +108,13 @@ export const Login = () => {
               </div>
             </RadioGroup>
           </div>
-          <Button type="submit" className="w-full my-4">
-            Login
-          </Button>
+          {
+            loading ? <Button className='w-full my-4'>
+              <Loader2 className='mr-2 h-4 w-4 animate-spin' />Please wait
+            </Button> : <Button type="submit" className="w-full my-4">
+              Login
+            </Button>
+          }
           <span className='text-sm'>Don't have an account?
             <Link to="/signup" className='text-sky-500'>
               Signup
