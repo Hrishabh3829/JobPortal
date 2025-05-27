78813aaeb4cebb19a12ef0fb2726db58914351bd
Hrishabh3829 <2200032748@kluniversity.in>
2025-05-27 13:04:59 +0530

    multer added in route

--- backend/route/user.route.js
+++ backend/route/user.route.js
@@ -1,10 +1,11 @@
 import express from "express"
 import { login, register, updateProfile, logout } from "../controllers/user.controller.js";
 import isAuthenticated from "../middlewares/isAuthenticated.js";
+import { singleMulter } from "../middlewares/multer.js";
 
 const router = express.Router();
 
-router.route("/register").post(register)
+router.route("/register").post(singleMulter,register)
 router.route("/login").post(login)
 router.route("/profile/update").post(isAuthenticated, updateProfile)
 router.route("/logout").get(logout)
