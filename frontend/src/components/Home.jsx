e6c837cb4fef25ae4e7661f01d62f177ce5c4a97
Hrishabh3829 <2200032748@kluniversity.in>
2025-05-28 01:55:56 +0530

    shadcn ui carousel design

--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import NavbarOne from './shared/NavbarOne'
 import { Herosection } from './Herosection'
+import { CategoryCarousel } from './CategoryCarousel'
 
 export const Home = () => {
   return (
     <div>
         <NavbarOne/>
         <Herosection/>
+        <CategoryCarousel/>
     </div>
   )
 }
