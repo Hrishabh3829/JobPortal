500e63031f6d5b67b7616596692e88fc75e796ae
Hrishabh3829 <2200032748@kluniversity.in>
2025-05-29 11:44:59 +0530

    changes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,9 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom"
-import NavbarOne from "./components/shared/NavbarOne.jsx"
 import { Login } from "./components/auth/Login"
 import { Signup } from "./components/auth/Signup"
 import { Home } from "./components/Home"
 import { Jobs } from "./components/auth/Jobs.jsx"
+import { Browse } from "./components/auth/Browse.jsx"
 
 
 
@@ -24,6 +24,10 @@ const appRouter =createBrowserRouter([
   {
     path:'/jobs',
     element:<Jobs/>
+  },
+  {
+    path:'/browse',
+    element:<Browse/>
   }
 ])
 
