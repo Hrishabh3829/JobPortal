9afb12c82dbfec7eed67909fbb4caad25286e2bc
Hrishabh3829 <2200032748@kluniversity.in>
2025-06-07 17:03:50 +0530

    profile done

--- frontend/src/components/Profile.jsx
+++ frontend/src/components/Profile.jsx
@@ -1,16 +1,24 @@
 import React from 'react'
-import NavbarOne from './shared/NavbarOne'
+import NavbarOne from './shared/NavbarOne.jsx'
 import { Avatar, AvatarImage } from '@radix-ui/react-avatar'
 import { Button } from './ui/button'
-import { Contact, Mail, Pen } from 'lucide-react'
+import { ArrowBigDownDash, Contact, Mail, Pen } from 'lucide-react'
 import { Badge } from './ui/badge'
+import { Label } from '@radix-ui/react-label'
+import { AppliedJobTable } from './AppliedJobTable.jsx'
+
+
+
+const skills = ["html", "java", "js", "express"]
 
 export const Profile = () => {
+    const isResume = true
+
     return (
-        <div>
+        <div >
             <NavbarOne />
 
-            <div className='max-w-6xl mx-30 bg-white border border-gray-200 rounded-2xl my-7 p-8'>
+            <div className='max-w-4xl mx-30 bg-white border border-gray-200 rounded-2xl my-7 p-8'>
                 <div className='flex items-center gap-4'>
                     <Avatar className='h-24 w-24'>
                         <AvatarImage
@@ -31,12 +39,34 @@ export const Profile = () => {
                     </span>
 
                 </div>
-                <div className='mx-10'>
+                <div className='mx-10 my-2 font-semibold'>
                     <h1>Skills</h1>
-                    {
-                        [1,2,3,4].map((item,index)=><Badge key={index}>{item}</Badge>)
-                    }
+                    {skills.length !== 0 ? (
+                        <div className="flex flex-wrap gap-2">
+                            {skills.map((item, index) => (
+                                <Badge key={index}>{item}</Badge>
+                            ))}
+                        </div>
+                    ) : (
+                        <span className='font-semibold'>NA</span>
+                    )}
                 </div>
+
+                <div className='grid w-full max-w-sm items-center gap-1.5 my-2 mx-10 '>
+                    <Label className='text-md font-bold'>Resume</Label>
+                    <div className='-my-2'>
+                        {
+                            isResume ? <a target='blank' href='https://hrishabhgupta.me' className='text-sky-500 hover:underline cursor-pointer transform transition-transform duration-200 hover:scale-110 inline-block'>Hey</a> : <span className='font-semibold'>NA</span>
+                        }
+                    </div>
+                </div>
+
+            </div>
+
+            <div className='max-w-4xl mx-auto bg-white rounded-2xl'>
+                <h1 className='text-purple-400 font-serif text-lg'>Applied Jobs</h1>
+                {/* Applied job Table */}
+                <AppliedJobTable />
             </div>
 
         </div>
