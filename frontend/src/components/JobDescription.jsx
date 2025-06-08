005202e00e38ad0e587be93ce1a0c7f153232b05
Hrishabh3829 <2200032748@kluniversity.in>
2025-06-08 14:45:29 +0530

    details page done

--- frontend/src/components/JobDescription.jsx
+++ frontend/src/components/JobDescription.jsx
@@ -9,7 +9,9 @@ export const JobDescription = () => {
         <div className='max-w-5xl mx-auto my-10'>
             <div className='flex items-center justify-between'>
                 <div>
-                    <h1 className='font-bold text-xl'>Title</h1>
+                    <div className='my-2'>
+                    <h1 className='font-bold text-xl'>Frontend Developer</h1>
+                    </div>
                     <div className='flex flex-wrap gap-2'>
                         <Badge className='text-blue-700 font-bold' variant='ghost'>12 Positions</Badge>
                         <Badge className='text-blue-700 font-bold' variant='ghost'>Part time</Badge>
@@ -28,7 +30,16 @@ export const JobDescription = () => {
                     {isApplied ? 'Already Applied' : 'Apply Now'}
                 </Button>
             </div>
-            <h1 className='border-b-2 border-b-gray-300 font-medium'>Job Description</h1>
+            <h1 className='border-b-2 border-b-gray-300 font-semibold p-4 '>Job Description</h1>
+            <div className='my-4'>
+                <h1 className='font-bold my-2 '>Role: <span className='pl-4 font-normal text-gray-800'>Frontend Developer</span></h1>
+                <h1 className='font-bold my-2 '>Location: <span className='pl-4 font-normal text-gray-800'>Hyderabad</span></h1>
+                <h1 className='font-bold my-2 '>Description: <span className='pl-4 font-normal text-gray-800'>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Provident ea rem perspiciatis impedit incidunt. Ex vitae at enim quo ullam! Magni ratione tenetur vel ea iste, consequuntur optio magnam earum?</span></h1>
+                <h1 className='font-bold my-2 '>Experience: <span className='pl-4 font-normal text-gray-800'>3 years</span></h1>
+                <h1 className='font-bold my-2 '>Salary: <span className='pl-4 font-normal text-gray-800'>10LPA</span></h1>
+                <h1 className='font-bold my-2 '>Total Applicants: <span className='pl-4 font-normal text-gray-800'>400</span></h1>
+                <h1 className='font-bold my-2 '>Posted Date: <span className='pl-4 font-normal text-gray-800'>08-06-2025</span></h1>
+            </div>
         </div>
     )
 }
