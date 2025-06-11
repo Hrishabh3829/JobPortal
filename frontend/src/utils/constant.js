7c799fe1a27ff215c9f95351b65491ac0da8d78c
Hrishabh3829 <2200032748@kluniversity.in>
2025-06-11 17:01:04 +0530

    redux

--- frontend/src/utils/constant.js
+++ frontend/src/utils/constant.js
@@ -1,3 +1,4 @@
 export const USER_API_END_POINT="http://localhost:8000/api/v1/user"
+export const JOB_API_END_POINT="http://localhost:8000/api/v1/job"
 // export const USER_API_END_POINT="https://rn8l9kd4-8000.inc1.devtunnels.ms/api/v1"
 
