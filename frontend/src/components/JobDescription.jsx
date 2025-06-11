1f6550e38ba9d5d1372c2045f5873a027c291e18
Hrishabh3829 <2200032748@kluniversity.in>
2025-06-12 00:10:38 +0530

    get all the jobs

--- frontend/src/components/JobDescription.jsx
+++ frontend/src/components/JobDescription.jsx
@@ -1,21 +1,50 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { Badge } from './ui/badge'
 import { Button } from './ui/button'
+import { useParams } from 'react-router-dom'
+import axios from 'axios'
+import { JOB_API_END_POINT } from '@/utils/constant'
+import { setSingleJob } from './redux/jobSlice'
+import { useDispatch, useSelector } from 'react-redux'
+// import store from './redux/store'
 
 export const JobDescription = () => {
-    const isApplied = true
+    const { singleJob } = useSelector(store => store.job)
+    const { user } = useSelector(store => store.auth)
+    const isApplied = singleJob?.applications?.some(application => application.applicant == user?._id) || false
+
+    const params = useParams()
+    const jobId = params.id;
+    // useGetSingleJob(jobId)//custom hook for single job
+    const dispatch = useDispatch()
+
+    useEffect(() => {
+        const fetchSingleJob = async () => {
+            try {
+                const res = await axios.get(`${JOB_API_END_POINT}/get/${jobId}`, { withCredentials: true });
+                if (res.data.success) {
+                    dispatch(setSingleJob(res.data.job))
+
+                }
+
+            } catch (error) {
+                console.log(error)
+            }
+        }
+        fetchSingleJob();
+    }, [jobId, dispatch, user?._id])
     return (
 
         <div className='max-w-5xl mx-auto my-10'>
             <div className='flex items-center justify-between'>
                 <div>
                     <div className='my-2'>
-                        <h1 className='font-bold text-xl'>Frontend Developer</h1>
+                        <h1 className='font-bold text-xl'>{singleJob?.title}</h1>
                     </div>
                     <div className='flex flex-wrap gap-2'>
-                        <Badge className='text-blue-700 font-bold' variant='ghost'>12 Positions</Badge>
-                        <Badge className='text-blue-700 font-bold' variant='ghost'>Part time</Badge>
-                        <Badge className='text-blue-700 font-bold' variant='ghost'>24 LPA</Badge>
+                        <Badge className='text-blue-700 font-bold' variant='ghost'>{singleJob?.position}Positions</Badge>
+                        <Badge className='text-blue-700 font-bold' variant='ghost'>{singleJob?.jobType}</Badge>
+                        <Badge className='text-blue-700 font-bold' variant='ghost'>{singleJob?.salary}LPA</Badge>
                     </div>
                 </div>
                 <Button disabled={isApplied}
@@ -32,13 +61,13 @@ export const JobDescription = () => {
             </div>
             <h1 className='border-b-2 border-b-gray-300 font-semibold p-4 '>Job Description</h1>
             <div className='my-4'>
-                <h1 className='font-bold my-2 '>Role: <span className='pl-4 font-normal text-gray-800'>Frontend Developer</span></h1>
-                <h1 className='font-bold my-2 '>Location: <span className='pl-4 font-normal text-gray-800'>Hyderabad</span></h1>
-                <h1 className='font-bold my-2 '>Description: <span className='pl-4 font-normal text-gray-800'>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Provident ea rem perspiciatis impedit incidunt. Ex vitae at enim quo ullam! Magni ratione tenetur vel ea iste, consequuntur optio magnam earum?</span></h1>
-                <h1 className='font-bold my-2 '>Experience: <span className='pl-4 font-normal text-gray-800'>3 years</span></h1>
-                <h1 className='font-bold my-2 '>Salary: <span className='pl-4 font-normal text-gray-800'>10LPA</span></h1>
-                <h1 className='font-bold my-2 '>Total Applicants: <span className='pl-4 font-normal text-gray-800'>400</span></h1>
-                <h1 className='font-bold my-2 '>Posted Date: <span className='pl-4 font-normal text-gray-800'>08-06-2025</span></h1>
+                <h1 className='font-bold my-2 '>Role: <span className='pl-4 font-normal text-gray-800'>{singleJob?.title}</span></h1>
+                <h1 className='font-bold my-2 '>Location: <span className='pl-4 font-normal text-gray-800'>{singleJob?.location}</span></h1>
+                <h1 className='font-bold my-2 '>Description: <span className='pl-4 font-normal text-gray-800'>{singleJob?.description}</span></h1>
+                <h1 className='font-bold my-2 '>Experience: <span className='pl-4 font-normal text-gray-800'>{singleJob?.experience}yrs</span></h1>
+                <h1 className='font-bold my-2 '>Salary: <span className='pl-4 font-normal text-gray-800'>{singleJob?.salary}LPA</span></h1>
+                <h1 className='font-bold my-2 '>Total Applicants: <span className='pl-4 font-normal text-gray-800'>{singleJob?.applications?.length}</span></h1>
+                <h1 className='font-bold my-2 '>Posted Date: <span className='pl-4 font-normal text-gray-800'>{singleJob?.createdAt.split("T")[0]}</span></h1>
             </div>
         </div>
     )
--- frontend/src/components/auth/Job.jsx
+++ frontend/src/components/auth/Job.jsx
@@ -6,14 +6,22 @@ import { Button } from '../ui/button'
 import { Badge } from '../ui/badge'
 import { useNavigate } from 'react-router-dom'
 
-export const Job = () => {
+export const Job = ({ job }) => {
 
     const navigate = useNavigate()
-    const jobId="7uyerwu68935"
+    // const jobId="7uyerwu68935"
+
+    const daysAgoFunction = (mongodbTime) => {
+        const createdAt = new Date(mongodbTime)
+        const currentTime = new Date()
+        const timeDifference = currentTime - createdAt
+        return Math.floor(timeDifference / (1000 * 24 * 60 * 60))
+
+    }
     return (
         <div className='p-6 rounded-md drop-shadow-xl bg-white border border-sky-200'>
             <div className='flex items-center justify-between'>
-                <p className='text-sm text-gray-500'>2 Days ago</p>
+                <p className='text-sm text-gray-500'>{daysAgoFunction(job?.createdAt) == 0 ? "Today" : `${daysAgoFunction(job?.createdAt)} days ago`}</p>
                 <Button variant="outline" className="rounded-full " size="icon">
                     <Bookmark />
                 </Button>
@@ -28,21 +36,21 @@ export const Job = () => {
                     </Avatar>
                 </Button>
                 <div>
-                    <h1 className='font-medium text-lg'>Google</h1>
+                    <h1 className='font-medium text-lg'>{job?.company?.name}</h1>
                     <p className='text-sm text-gray-500'>India</p>
                 </div>
             </div>
             <div>
-                <h1 className='font-bold text-lg my-2'>Backend Developer</h1>
-                <p className='text-sm text-gray-600'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit quam dolorum illum voluptas maxime numquam a sint architecto non ducimus vitae, perspiciatis corporis temporibus voluptates dolore iste tenetur doloribus consequuntur?</p>
+                <h1 className='font-bold text-lg my-2'>{job?.title}</h1>
+                <p className='text-sm text-gray-600'>{job?.description}</p>
             </div>
             <div className='flex flex-wrap gap-2'>
-                <Badge className='text-blue-700 font-bold' variant='ghost'>12 Positions</Badge>
-                <Badge className='text-blue-700 font-bold' variant='ghost'>Part time</Badge>
-                <Badge className='text-blue-700 font-bold' variant='ghost'>24 LPA</Badge>
+                <Badge className='text-blue-700 font-bold' variant='ghost'>{job?.position}Positions</Badge>
+                <Badge className='text-blue-700 font-bold' variant='ghost'>{job?.jobType}</Badge>
+                <Badge className='text-blue-700 font-bold' variant='ghost'>{job?.salary}LPA</Badge>
             </div>
             <div className='flex items-center gap-4 mt-4'>
-                <Button variant="outline" className="rounded-full bg-sky-100" onClick={() => navigate(`/description/${jobId}`)}>Details</Button>
+                <Button variant="outline" className="rounded-full bg-sky-100" onClick={() => navigate(`/description/${job?._id}`)}>Details</Button>
                 <Button variant="outline" className="rounded-full bg-sky-200">Save for later</Button>
             </div>
 
