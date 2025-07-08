9ba65c9840b39e0f7a91fe2a9b4580fb59a703c5
Hrishabh3829 <2200032748@kluniversity.in>
2025-07-08 18:36:24 +0530

    frontend url added

--- backend/index.js
+++ backend/index.js
@@ -22,7 +22,7 @@ const corsOptions = {
         ? [process.env.FRONTEND_URL || "https://your-production-frontend-url.com"] 
         : [
             "https://rn8l9kd4-5173.inc1.devtunnels.ms",
-            "http://localhost:5173"
+            
           ],
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
